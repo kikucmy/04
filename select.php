02d97a8ae0d7d52dca239b3e7744ec53e1a8a72c
kikucmy <poko.mk13@river.ocn.ne.jp>
2020-12-05 16:13:16 +0900

    課題提出

--- select.php
+++ select.php
@@ -1,1 +1,34 @@
 <?php
+
+define('DSN', 'mysql:host=db;dbname=pet_shop;charset=utf8;');
+define('USER', 'staff');
+define('PASSWORD', '9999');
+
+try {
+    $dbh = new PDO(DSN, USER, PASSWORD);
+} catch (PDOException $e) {
+    echo $e->getMessage();
+    exit;
+}
+
+// SQL文の組み立て
+$sql = 'SELECT * FROM animals';
+
+// プリペアドステートメントの準備
+// $dbh->query($sql) でも良い
+$stmt = $dbh->prepare($sql);
+
+// プリペアドステートメントの実行
+$stmt->execute();
+
+// 結果の受け取り
+$animals = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+// var_dump($animals);
+foreach ($animals as $animal) {
+    echo $animal['type'] . 'の' . $animal['classifcation'] . 'ちゃん<br>';
+    echo $animal['description'] . '<br>';
+    echo $animal['birthday'] . ' 生まれ<br>';
+    echo '出身地 ' . $animal['birthplace'] . '<br>';
+    echo '<hr>';
+}
